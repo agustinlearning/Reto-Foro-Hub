e9e9db4b642996985c341c740269d6c9addd8406
agustin <agustinalvarez7378@gmail.com>
2025-07-17 15:52:29 -0400

    Adding logic to Topico, method Topico, method actualizarDatos and method cerrarTopico

--- Foro-Hub/src/main/java/alura_cursos/Foro_Hub/domain/topico/Topico.java
+++ Foro-Hub/src/main/java/alura_cursos/Foro_Hub/domain/topico/Topico.java
@@ -1,11 +1,15 @@
 package alura_cursos.Foro_Hub.domain.topico;
 
 import alura_cursos.Foro_Hub.domain.model.Estado;
+import alura_cursos.Foro_Hub.domain.model.curso.Curso;
+import alura_cursos.Foro_Hub.domain.model.usuario.Usuario;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Table(name = "Topicos")
 @Entity(name = "Topico")
@@ -18,11 +22,44 @@ public class Topico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String titulo;
     private String mensaje;
-    private String fechaDeCreacion;
-    private Estado estado;
-    private String autor;
-    private String curso;
+    private LocalDateTime fechaCreacion = LocalDateTime.now();
 
+    @Enumerated(EnumType.STRING)
+    private Estado estado = Estado.ABIERTO;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "autor_id")
+    private Usuario autor;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "curso_id")
+    private Curso curso;
+
+    public Topico(String titulo, String mensaje, Usuario autor, Curso curso) {
+        this.titulo = titulo;
+        this.mensaje = mensaje;
+        this.autor = autor;
+        this.curso = curso;
+        this.fechaCreacion = LocalDateTime.now();
+        this.estado = Estado.ABIERTO;
+    }
+
+    public void actualizarDatos(String titulo, String mensaje, Estado estado) { // mas tarde cambbiar por el dto de topicos
+        if (titulo != null && !titulo.isBlank()) {
+            this.titulo = titulo;
+        }
+        if (mensaje != null && !mensaje.isBlank()) {
+            this.mensaje = mensaje;
+        }
+        if (estado != null) {
+            this.estado = estado;
+        }
+    }
+
+    public void cerrarTopico() {
+        this.estado = Estado.CERRADO;
+    }
 }
--- Foro-Hub/src/main/resources/db/migration/V1__create-table-topicos.sql
+++ Foro-Hub/src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -2,7 +2,7 @@ create table topicos(
     id bigint not null auto_increment,
     titulo varchar(100) not null,
     mensaje varchar(100) not null,
-    fechaDeCreacion varchar(100) not null,
+    fechaCreacion varchar(100) not null,
     status varchar(100) not null,
     autor varchar(100) not null,
     curso varchar(100) not null,
