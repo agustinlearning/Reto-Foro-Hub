a9ab25a3c68e4a14d71675521f2baed0132ea9f0
agustin <agustinalvarez7378@gmail.com>
2025-07-17 15:34:36 -0400

    Creating Usuario and UsuarioRepository.java

---
+++ Foro-Hub/src/main/java/alura_cursos/Foro_Hub/domain/model/usuario/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package alura_cursos.Foro_Hub.domain.model.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
