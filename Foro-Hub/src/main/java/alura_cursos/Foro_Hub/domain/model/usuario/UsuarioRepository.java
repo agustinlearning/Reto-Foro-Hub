7a5a35f93bbcd7ea58446b1471b48522cb891f81
agustin <agustinalvarez7378@gmail.com>
2025-07-22 10:34:55 -0400

    creating AutenticacionService and method findByEmail in UsuarioRepository

--- Foro-Hub/src/main/java/alura_cursos/Foro_Hub/domain/model/usuario/UsuarioRepository.java
+++ Foro-Hub/src/main/java/alura_cursos/Foro_Hub/domain/model/usuario/UsuarioRepository.java
@@ -1,9 +1,14 @@
 package alura_cursos.Foro_Hub.domain.model.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 
+//    UserDetails findByEmail(String email);
 }
